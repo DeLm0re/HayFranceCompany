c38005aa774cca7217b831ecfa13f6b256d19588
marechal230eme <marechal230eme@gmail.com>
2017-06-12 23:21:57 +0200

    Fin connection

--- includes/i_connexion.php
+++ includes/i_connexion.php
@@ -4,21 +4,21 @@
     <form>
         <p>Connexion<p> 
             
-       
+        
             
-        <span id="erreur_email" class="erreur"></span>
-        <p>Adresse mail : <input id="email" type="text" spellcheck="false" onchange="request_connexion(readData_connexion,'email');"></p>
-            <span id="ok_email" style="display: none;"><img src="../images/ok.png"/></span>
-            <span id="ko_email" style="display: none;"><img src="../images/ko.png"/></span>
+        <span id="erreur_email_connexion" class="erreur"></span>
+        <p>Adresse mail : <input id="email_connexion" type="text" spellcheck="false" onchange="request_connexion(readData_connexion,'email_connexion');"></p>
+            <span id="ok_email_connexion" style="display: none;"><img src="../images/ok.png"/></span>
+            <span id="ko_email_connexion" style="display: none;"><img src="../images/ko.png"/></span>
         
-        <span id="erreur_mdp" class="erreur"></span>
-        <p>Mot de passe : <input id="mdp" type="password" onchange="request_connexion(readData_connexion,'mdp');"></p>
-            <span id="ok_mdp" style="display: none;"><img src="../images/ok.png"/></span>
-            <span id="ko_mdp" style="display: none;"><img src="../images/ko.png"/></span>
+        <span id="erreur_mdp_connexion" class="erreur"></span>
+        <p>Mot de passe : <input id="mdp_connexion" type="password" onchange="request_connexion(readData_connexion,'mdp_connexion');"></p>
+            <span id="ok_mdp_connexion" style="display: none;"><img src="../images/ok.png"/></span>
+            <span id="ko_mdp_connexion" style="display: none;"><img src="../images/ko.png"/></span>
         
         
         
-        <span id="loader" style="display: none;"><img id="img_loader" style="width: 10%;" src="../images/loader.gif" alt="Chargement" /></span>
+        <span id="loader_connexion" style="display: none;"><img id="img_loader" style="width: 10%;" src="../images/loader.gif" alt="Chargement" /></span>
     </form>
-    <input id="button" type="button" value="Se connecter" onclick="request2_connexion(readData_connexion);">
+    <input id="button_connexion" type="button" value="Se connecter" onclick="request2_connexion(readData_connexion);">
 </fieldset>

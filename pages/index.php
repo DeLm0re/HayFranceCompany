58c7dd6193996a6a204cae651cdbc8c02376db71
Antoine PARANT <antoine.parant83@gmail.com>
2017-06-13 11:25:44 +0200

    Dernière Modif Footer
    Réajustement du CSS et du HTML du footer

--- pages/index.php
+++ pages/index.php
@@ -1,26 +1,27 @@
 <html>
     <head>
         <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui"/>
-            <link href="../css/index.css" rel="stylesheet" type="text/css"/>
-            <link href="../css/overlay.css" rel="stylesheet" type="text/css"/>
-            <link href="../css/carte_overlay.css" rel="stylesheet" type="text/css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui"/>
+        <link href="../css/index.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/overlay.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/carte_overlay.css" rel="stylesheet" type="text/css"/>
     </head>
- 
+
+
     <body>
-        
+
         <?php
-            /*inclusion de l'overlay*/
-            include '../includes/i_overlay.php';
-            
-            /*inclusion du formulaire d'inscription*/
-            include '../includes/i_inscription.php';
+        /* inclusion de l'overlay */
+        include '../includes/i_overlay.php';
+
+        /* inclusion du formulaire d'inscription */
+        include '../includes/i_inscription.php';
         ?>
-        
+
         <span>Votre département : </span>
         <span id="mon_departement"></span></br>
         <a href="carte.php">carte</a>
-        
+
         <!-- SCRIPTS POUR L'AJAX -->
         <script src="../js/ajax.js" type="text/javascript"></script>
         <script src="../js/oXHR.js" type="text/javascript"></script>
@@ -28,12 +29,12 @@
         <!-- SCRIPTS POUR L'overlay -->
         <script src="../js/overlay.js" type="text/javascript"></script>
         <script src="../js/polyfill.js" type="text/javascript"></script>
-        
-        
+
+
 
     </body>
     <?php
-        include '../includes/i_footer.php'
+    include '../includes/i_footer.php'
     ?>
-    
+
 <html>

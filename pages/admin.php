8ae9a09a9a804e71cb00b20608ddd2f9bd027ada
VincentPetrini <vincent.petrini@isen.yncrea.fr>
2017-06-14 10:37:24 +0200

    Résolution de conflits

--- pages/admin.php
+++ pages/admin.php
@@ -1,85 +1,92 @@
+<?php
+
+include_once '../objet/session_objet.php';
+include_once '../objet/administration/o_admin.php';
+$admin = new Admin($bdd);
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui"/>
+    </head>
 
 
-<table>
-    <h1>Produits</h1>
-    <form action="ModificationProduit.php">
-    <input type="submit" value="Créer Produit" />
-    </form
-   <tr>
-       <th>ID</th>
-       <th>Nom Produit</th>
-       <th>Courte Description</th>
-       <th>Animaux</th>
-       <th>Nombre Image</th>
-       <th>Prix</th>
-   </tr>
-   
-   <?php
-     $tableauDonnees = recupDonneProduit();
-     $nombreLignes = count($tableauDonnees);
-     for($i=0 ; $i < $nombreLignes ; $i+=1 ){
-          $Colonne = donneesColonne($i, $tableauDonnees);
-          creerLigneTableau($Colonne);
-     }
-    
-   ?>
-   </a>
-</table>
+    <body>
+        <table>
+            <h1>Produits</h1>
+            <form action="ModificationProduit.php">
+            <input type="submit" value="Créer Produit" />
+            </form>
+           <tr>
+               <th>ID</th>
+               <th>Nom Produit</th>
+               <th>Courte Description</th>
+               <th>Prix</th>
+               <th>Nombre Image</th>
+               <th>Animaux</th>
+           </tr>
+           <?php
+                $listeProduit = $admin->consulteListeProduit();
+                afficheTableauProduit($listeProduit);
+           ?>
+        </table>
+    </body>
+</html>
 
 <?php
-function recupDonneProduit(){
- $bdd = new PDO('mysql:host=localhost;dbname=hayfrance;charset=utf8', 'root', '');
-	       
-        $req1 = $bdd->prepare("SELECT id_produit, nom_produit, description, description_rapide, prix_tonne FROM produit ");
-        
-        $req1->execute();
-        //var_dump($reqtest);
-        $tableauDonnees = array();
-        $i = 0 ; 
-	while ($back = $req1->fetch()) {
-             $tableauDonnees[$i] = $back ; 
-             $i = $i + 1 ;
-	} 
-    return($tableauDonnees) ; 
-}
-
-function donneesColonne($ligne, $TableauDonnes){
-    return($TableauDonnes[$ligne]) ; 
-}
-
-function creerLigneTableau($donneesColonne){
-    //URL de la page modification produit
-    $URL = "ModificationProduit.php";
-    $Colonne = array();
-    //On fait le même tableau que $donnesColonne mais on remplace la case
-    //avec le nom par le nom avec URL
-    for($i=0 ; $i <5 ; $i+=1){
-        if($i == 1){
-            $id = $donneesColonne["id_produit"];
-            $nom = $donneesColonne["nom_produit"];
-            $Colonne[$i] = "<a href=$URL?ID=$id > $nom </a>" ; 
-        }
-        else {
-            $Colonne[$i] = $donneesColonne[$i] ; 
-        }
-    }
-     
-    echo"<div>";
-    echo"<tr>";
-
-    for($i = 0 ; $i<count($Colonne) ; $i+=1)
-    {
-            ajoutCellule($Colonne[$i]);
-    }
-
-    echo"</tr>";
-    echo"</div>";
-    echo"</a>" ; 
-}
-
-function ajoutCellule($contenue)
+function afficheTableauProduit($liste)
 {
-    echo"<td>";
-    echo$contenue ;
-    echo"</td>"; 
+    $max = count($liste);
+    for($i = 0; $i < $max; $i++)
+    {
+        $produit = $liste[$i];
+        $infos = $produit->infos();
+        unset($infos['description']);
+        
+        //Récupère les infos du produit
+        $infos2 = array_values($infos);
+        //Ajoute le nombre d'images
+        array_push($infos2, count($produit->getNomImages()));
+        //Ajoute les catégories
+        array_push($infos2, animauxToString($produit->getCategories()));
+        //Affiche le tout
+        afficheProduit($infos2);
+    }
 }
+
+function afficheProduit($infos)
+{
+    $URL = "ModificationProduit.php";
+    $max = count($infos);
+    
+    echo '<tr>';
+    for($i = 0; $i < $max; $i++)
+    {
+        if($i == 1)
+        {
+            ajouteCellule("<a href=$URL?ID=$infos[0] > $infos[$i] </a>");
+        }
+        else
+        {
+            ajouteCellule($infos[$i]);   
+        }
+    }
+    echo '</tr>';
+}
+
+function ajouteCellule($cellule)
+{
+    echo "<td>$cellule</td>";
+}
+
+function animauxToString($animaux)
+{
+    $string = '';
+    foreach ($animaux as $animal)
+    {
+        $string = $string . ' ' . $animal['nom_categorie'];
+    }
+    return $string;
+}
+
+ 

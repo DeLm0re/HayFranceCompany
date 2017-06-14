d07e92d53bd99b08ac93da30001f11b61b536881
DeLm0re <jacquiez.romain@orange.fr>
2017-06-14 12:02:32 +0200

    modif loader/images

--- pages/MonCompte.php
+++ pages/MonCompte.php
@@ -8,7 +8,7 @@
     <head>
         <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui"/>
-            <link href="../css/i_inscription.css" rel="stylesheet" type="text/css"/>
+            <link href="../css/i_inscription_connexion.css" rel="stylesheet" type="text/css"/>
     </head>
  
       <body>

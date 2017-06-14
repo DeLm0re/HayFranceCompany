3832b2ecc9de5c56e5e70a33392dea428b1ed336
DeLm0re <jacquiez.romain@orange.fr>
2017-06-14 12:16:22 +0200

    loader

--- includes/i_modifie_compte.php
+++ includes/i_modifie_compte.php
@@ -54,7 +54,7 @@
             <span id="ok_dpt" style="display: none;"><img src="../images/ok.png"/></span>
             <span id="ko_dpt" style="display: none;"><img src="../images/ko.png"/></span>
             
-        <span id="loader" style="display: none;"><img id="img_loader" style="width: 10%;" src="../images/loader.gif" alt="Chargement" /></span>
+        <span id="loader" style="display: none;"><img id="img_loader" src="../images/loader.gif" alt="Chargement" /></span>
     </form>
     <input id="button" type="button" value="Modifier" onclick="request2(readData);">
    

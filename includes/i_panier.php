d6c7e883e75abf74130863e1160e5f739f035bae
DeLm0re <jacquiez.romain@orange.fr>
2017-06-15 15:33:20 +0200

    Revert "Revert "rajout page panier""
    This reverts commit 17c648cd6fc64791f0b7897c2dfc9647045fd454.

---
+++ includes/i_panier.php
@@ -0,0 +1,58 @@
+<!-- div class="div_ensemble_panier" -->
+
+<div class="div_titre_panier">
+    <span class="titre_panier">Votre panier</span><br>
+</div>
+
+<div class="div_panier">
+    <div class="div_produit_panier">
+        <div class="produit_panier">
+            <div class="element_produit_panier">
+                <img class="image_produit_panier" src="../images/foin1.png">
+            </div>
+            <div class="element_produit_panier">
+                <p class="nom_produit_panier">Foin de Crau</p>
+            </div>
+        </div>
+        <div class="produit_panier">
+            <div class="element_produit_panier">
+                <img class="image_produit_panier" src="../images/foin2.png">
+            </div>
+            <div class="element_produit_panier">
+                <p class="nom_produit_panier">Timothy</p>
+            </div>
+        </div>
+        <div class="produit_panier">
+            <div class="element_produit_panier">
+                <img class="image_produit_panier" src="../images/foin1.png">
+            </div>
+            <div class="element_produit_panier">
+                <p class="nom_produit_panier">Foin de Longchamp</p>
+            </div>
+        </div>
+        <div class="produit_panier">
+            <div class="element_produit_panier">
+                <img class="image_produit_panier" src="../images/foin2.png">
+            </div>
+            <div class="element_produit_panier">
+                <p class="nom_produit_panier">Luzerne</p>
+            </div>
+        </div>
+    </div>
+    <div class="div_prix_panier">
+        <div class="prix_panier">
+            <span class="span_prix_panier">Prix total : </span>
+            <span class="le_prix_panier">500 €</span>
+        </div>
+    </div>
+</div>
+
+<div class="div_button_panier">
+    <input class="button_panier" type="button" onclick="#" value="Commander">
+</div>
+
+<!-- /div -->
+
+
+
+

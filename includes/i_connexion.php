1f5ad1befed2e13f2596717d846049cebdd50e56
DeLm0re <jacquiez.romain@orange.fr>
2017-06-15 09:05:18 +0200

    formulaire et css

--- includes/i_connexion.php
+++ includes/i_connexion.php
@@ -1,24 +1,41 @@
-  <span id="erreur_connexion" class="erreur"></span>
-  <span id="validation_connexion" class="erreur"></span>
-<fieldset id="formulaire_connexion">
-    <form>
-        <p>Connexion<p> 
-            
-        
-      
-        <span id="erreur_email_connexion" class="erreur"></span>
-        <p>Adresse mail : <input id="email_connexion" type="text" spellcheck="false" onchange="request_connexion(readData_connexion,'email_connexion');"></p>
-            <span id="ok_email_connexion" style="display: none;"><img src="../images/ok.png"/></span>
-            <span id="ko_email_connexion" style="display: none;"><img src="../images/ko.png"/></span>
-        
-        <span id="erreur_mdp_connexion" class="erreur"></span>
-        <p>Mot de passe : <input id="mdp_connexion" type="password" onchange="request_connexion(readData_connexion,'mdp_connexion');"></p>
-            <span id="ok_mdp_connexion" style="display: none;"><img src="../images/ok.png"/></span>
-            <span id="ko_mdp_connexion" style="display: none;"><img src="../images/ko.png"/></span>
-        
-        
-        
+<form id="formulaire">
+    <div class="div_titre">
+    </div>
+
+    <div class="div_erreur_validation">
+        <span id="erreur_connexion" class="champ_erreur" style="display: none;"></span>
+        <span id="validation_connexion" class="champ_validation" style="display: none;"></span>
+    </div>
+
+    <div class="div_ensemble_champ">
+
+        <div id="div_champ_erreur_email_connexion" style="display: none;">
+            <span id="erreur_email_connexion" class="champ_erreur"></span><br>
+        </div>
+        <div class="div_champ">
+            <span class="champ">Adresse mail : <input id="email_connexion" type="text" spellcheck="false" onchange="request_connexion(readData_connexion, 'email_connexion');">
+                <span id="ok_email_connexion" style="display: none;"><img class="icon_preverif" src="../images/ok.png"/></span>
+                <span id="ko_email_connexion" style="display: none;"><img class="icon_preverif" src="../images/ko.png"/></span>
+            </span><br>
+        </div>
+
+        <div id="div_champ_erreur_mdp_connexion" style="display: none;">
+            <span id="erreur_mdp_connexion" class="champ_erreur"></span><br>
+        </div>
+        <div class="div_champ">
+            <span class="champ">Mot de passe : <input id="mdp_connexion" type="password" onchange="request_connexion(readData_connexion, 'mdp_connexion');">
+                <span id="ok_mdp_connexion" style="display: none;"><img class="icon_preverif" src="../images/ok.png"/></span>
+                <span id="ko_mdp_connexion" style="display: none;"><img class="icon_preverif" src="../images/ko.png"/></span>
+            </span><br>
+        </div>
+    </div>
+
+    <div class="div_loader_connexion">
         <span id="loader_connexion" style="display: none;"><img id="img_loader" src="../images/loader.gif" alt="Chargement" /></span>
-    </form>
-    <input id="button_connexion" type="button" value="Se connecter" onclick="request2_connexion(readData_connexion);">
-</fieldset>
+    </div>
+
+    <div class="div_button">
+        <input id="button_connexion" type="button" value="Se connecter" onclick="request2_connexion(readData_connexion);">
+    </div>
+</form>
+
--- pages/inscription_connexion.php
+++ pages/inscription_connexion.php
@@ -1,39 +1,50 @@
 <?php
-    //inclusion de la session et des objets
-    include_once '../objet/session_objet.php';
-    $user = new Utilisateur($bdd);
-    demarreSession($user);
+//inclusion de la session et des objets
+include_once '../objet/session_objet.php';
+$user = new Utilisateur($bdd);
+demarreSession($user);
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui"/>
-            <link href="../css/i_inscription_connexion.css" rel="stylesheet" type="text/css"/>
-           
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui"/>
+        <link href="../css/i_inscription_connexion.css" rel="stylesheet" type="text/css"/>
+
     </head>
- 
+
     <body>
         <div class="page">
-	<div class="bloc-principal">
-           
-        <?php
-            include '../includes/i_navbar.php';
-            /*inclusion du formulaire de connexion*/
-            include '../includes/i_connexion.php';
-            
-            include '../includes/i_inscription.php';    
-        ?>
-          
-        <!-- SCRIPTS POUR L'AJAX -->
-        <script src="../js/a_connexion.js" type="text/javascript"></script>
-        <script src="../js/a_inscription.js" type="text/javascript"></script> 
-        <script src="../js/oXHR.js" type="text/javascript"></script>
-    
-        <div class="clear"></div>
-	</div><!-- fin bloc-principal -->
-	<?php
+            <div class="bloc-principal">
+                <?php
+                include '../includes/i_navbar.php';
+                ?>
+                <div class="div_inscription_connexion">
+                    <div class="div_connexion">
+                        <?php
+                        /* inclusion du formulaire de connexion */
+                        include '../includes/i_connexion.php';
+                        ?>
+                    </div>
+                    <div class="div_inscription">
+                        <?php
+                        /* inclusion du formulaire d'inscription */
+                        include '../includes/i_inscription.php';
+                        ?>
+                    </div>
+                </div>
+
+                <div class="clear"></div>
+                
+            </div><!-- fin bloc-principal -->
+            <?php
             include '../includes/i_footer.php';
-        ?>
+            ?>
+
+            <!-- SCRIPTS POUR L'AJAX -->
+            <script src="../js/a_connexion.js" type="text/javascript"></script>
+            <script src="../js/a_inscription.js" type="text/javascript"></script> 
+            <script src="../js/oXHR.js" type="text/javascript"></script>
+            
         </div><!-- fin page -->
     </body>
 </html>

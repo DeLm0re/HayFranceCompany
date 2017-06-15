19b3e062b20c3c6cf8344c03a18f72b48c4384be
marechal230eme <marechal230eme@gmail.com>
2017-06-15 10:21:07 +0200

    fin formulaire d'ajout produit

--- includes/i_formulaire_produit.php
+++ includes/i_formulaire_produit.php
@@ -15,26 +15,26 @@ $id = $_GET['id_produit'];
 
 <form class="formulaire_produit" >
 
-    <p>Format des balles CHC : <input id="Format22" type="radio" name="Format" value="22" checked="true">
+    <span> Format des balles CHC : <input id="Format22" type="radio" name="Format" value="22" checked="true">
         <label for="Format22">22 kg</label>
         <input id="Format32" type="radio" name="Format" value="32"><label for="Format32">32 kg</label>
-    </p>
-
-
-    <span id="erreur_nbr_pallette" class="erreur" ></span>
+    </span><br>
     
     <!-- passage de l'id du produit en mode shlag !! a changé immediatement -->
-    <p> <input type="number" id="id_produit" value="<?php echo $id  ;?>" style="display: none;"  onchange="request(readData, 'value_produit');"> </p>
+    <span> <input type="number" id="id_produit" value="<?php echo $id  ;?>" style="display: none;"  onchange="request(readData, 'id_produit');"> </span>
     <!-- ***************************************************************************************************-->
-    <p>Nombre de pallette : <input id="nbr_pallette" type="number" min="1" max="8"  onchange="request(readData, 'nbr_pallette');"></p>
+    
+    <span id="erreur_nbr_pallette" class="erreur" ></span><br>
+    <span> Nombre de pallette : <input id="nbr_pallette" type="number" min="1" max="8"  onchange="request(readData, 'nbr_pallette');"></span><br>
     <span id="ok_nbr_pallette" style="display: none;"><img src="../images/ok.png"/></span>
     <span id="ko_nbr_pallette" style="display: none;"><img src="../images/ko.png"/></span>
 
-
-    <span id="loader" style="display: none;"><img id="img_loader" style="width: 10%;" src="../images/loader.gif" alt="Chargement" /></span>
-    <input id="button" type="button" value="Ajouter ce produit à mon panier" onclick="request2(readData);">
     
-     <span id="confirmation commande" class="erreur" ></span>
+    <span id="loader" style="display: none;"><img id="img_loader" style="width: 10%;" src="../images/loader.gif" alt="Chargement" /></span><br>
+    <input id="button" type="button" value="Ajouter ce produit à mon panier" onclick="request2(readData);"><br>
+    
+     <span id="confirmation commande" class="erreur" ></span><br>
+     <span id="erreur_connecter" class="erreur" ></span>
 </form>
 
 
--- js/a_formulaire_produit.js
+++ js/a_formulaire_produit.js
@@ -9,10 +9,12 @@ function request(callback,champ) {
     };
 
     var contenu = encodeURIComponent(document.getElementById(champ).value);
-   //alert(contenu);
-   //alert(champ);
+    var id_produit = encodeURIComponent(document.getElementById("id_produit").value);
+  
+  // alert(contenu);
+  //  alert(champ);
  //   alert("on passe par là")
-    xhr.open("GET", "../ajax/a_verif_formulaire_produit.php?contenu="+contenu+"&champ="+champ, true);
+    xhr.open("GET", "../ajax/a_verif_formulaire_produit.php?contenu="+contenu+"&champ="+champ+"&id_produit="+id_produit, true);
     xhr.send(null);
 }
 
@@ -45,7 +47,8 @@ function request2(callback) {
     else{
         Format = encodeURIComponent(document.getElementById("Format32").value);
     }
-  //  alert(nbr_pallette);
+    //alert(nbr_pallette)
+    //alert(id_produit);
     
     xhr.open("GET", "../ajax/a_verif_formulaire_produit.php?champ="+champ+"&nbr_pallette="+nbr_pallette+"&Format="+Format+"&id_produit="+id_produit +"", true);
     xhr.send(null);
@@ -53,6 +56,7 @@ function request2(callback) {
 
 function readData(data,champ)
 {
+    //alert(data) ; 
     if (data === "OK")
     {
         document.getElementById("ok_"+champ).style.display = "inherit";
@@ -64,20 +68,22 @@ function readData(data,champ)
         document.getElementById("ko_"+champ).style.display = "inherit";
     }
     
-   
     if (champ === 'button')
     {
         //On vide les spans d'erreur qui se sont remplis pour l'erreur précedente
         vide_span_erreur('nbr_pallette');
-        
-     
-           alert(data); 
+       //    alert(data); 
         
         //On affiche l'erreur au dessus du champ la contenant
+        if (data === "NonCo")
+        {
+            document.getElementById("erreur_connecter").innerHTML = "Vous n'etes pas connecter veillez vous connecter ";
+            document.location.href = "http://localhost/HayFranceCompany/pages/inscription_connexion.php";
+        }
         if (data === "erreur_nbr_pallette")
-            document.getElementById("erreur_nbr_pallette").innerHTML = "le nombre de pallette est incorect ";
-          if (data === "ajoutT")
-           document.getElementById("confirmation commande").innerHTML = " votre produits a été ajouter au panier  ";
+            document.getElementById("erreur_nbr_pallette").innerHTML = "Le nombre de pallette maximum par produit est de 8 ";
+        if (data === "ajoutT")
+           document.getElementById("confirmation commande").innerHTML = "Votre produits a été ajouter au panier  ";
     }
 }
 
